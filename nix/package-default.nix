73e25e08b6416b03eb180686321a22975a36b9c0
cristianoliveira <contato@cristianoliveira.com.br>
2025-05-27 08:03:14 +0200

    chore(nix): bump version to v0.0.2

--- nix/package-default.nix
+++ nix/package-default.nix
@@ -3,7 +3,7 @@
     # name of our derivation
     name = "aerospace-scratchpad";
     # FIXME: once we have the first release, we can use the version
-    version = "v0.0.1";
+    version = "v0.0.2";
     # version = "64ebad853115b0565f284354efb833bf6c045c33";
 
     # sources that will be used for our derivation.
@@ -11,7 +11,7 @@
       owner = "cristianoliveira";
       repo = "aerospace-scratchpad";
       rev = version;
-      sha256 = "sha256-hkPNzJaAOsaKJXZ/C4sFuMfKTFQxCSIk2Z1th9Hsha0=";
+      sha256 = "sha256-50iovhMLHn/IvdqTgixj6dK4An9cHvy+8nEYg6QdQag=";
     };
 
     vendorHash = "sha256-h/GtBDJOOyVXfSv/o4hozZAcHPlg2uJLApy5r3WP9aE=";
