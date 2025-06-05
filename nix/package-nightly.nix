b381029140124ac41a9772964a321ab735c7b741
cristianoliveira <3959744+cristianoliveira@users.noreply.github.com>
2025-06-05 00:19:04 +0000

    chore(nix): bump nightly hashes and version

--- nix/package-nightly.nix
+++ nix/package-nightly.nix
@@ -9,7 +9,7 @@
       owner = "cristianoliveira";
       repo = "aerospace-scratchpad";
       rev = version;
-      sha256 = "sha256-obB1qRZyX85qoHLgr9CkiS5yPNhK0st6SkG3cKlDTLo=";
+      sha256 = "sha256-xJ71k+y21vpxxXPUurWpQa9jC+xbUH5DMKPYLX6A74o=";
     };
 
     vendorHash = "sha256-CN/51oTiUm7oGuZY8eMb+8Dc4jA4B6Oqvu6khxOldlo=";
