7270a1e9da5d6f1996ff36a2480b390b7e616647
cristianoliveira <contato@cristianoliveira.com.br>
2025-06-26 17:45:10 +0200

    tests: add special test logger for debugging

---
+++ internal/testutils/logger.go
@@ -0,0 +1,37 @@
+package testutils
+
+import (
+	"github.com/cristianoliveira/aerospace-scratchpad/internal/logger"
+)
+
+
+type TestingLogger struct{
+	Logger func(msg string, args ...any)
+}
+
+func (l *TestingLogger) LogInfo(msg string, args ...any) {
+	l.Logger(msg, args...)
+}
+func (l *TestingLogger) LogError(msg string, args ...any) {
+	l.Logger(msg, args...)
+}
+func (l *TestingLogger) LogDebug(msg string, args ...any) {
+	l.Logger(msg, args...)
+}
+func (l *TestingLogger) Close() error {
+	// No-op
+	return nil
+}
+func (l *TestingLogger) GetConfig() logger.LogConfig {
+	// No-op
+	return logger.LogConfig{
+		Path:  "/tmp/aerospace-marks.log",
+		Level: "DISABLED",
+	}
+}
+func (l *TestingLogger) AsJson(data any) string {
+	// No-op
+	return ""
+}
+
+
