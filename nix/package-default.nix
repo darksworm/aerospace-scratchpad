3b4169f2886125f6fd29d07d459b117c81743a7c
cristianoliveira <3959744+cristianoliveira@users.noreply.github.com>
2025-06-27 20:40:31 +0000

    chore(nix): bump nix hashes and version

--- nix/package-default.nix
+++ nix/package-default.nix
@@ -3,15 +3,15 @@
     # name of our derivation
     name = "aerospace-scratchpad";
     # FIXME: once we have the first release, we can use the version
-    version = "v0.1.3";
-    # version = "v0.1.3";
+    version = "v0.1.4";
+    # version = "v0.1.4";
 
     # sources that will be used for our derivation.
     src = pkgs.fetchFromGitHub {
       owner = "cristianoliveira";
       repo = "aerospace-scratchpad";
       rev = version;
-      sha256 = "sha256-5SKHdlx3B95QISTgSBMDTYPq6dqQVjsUhVeS4c7LtlA=";
+      sha256 = "sha256-3kt7WtL48UoXU+6c+4I7g4HpRi5iwm280+pX+26Oln8=";
     };
 
     vendorHash = "sha256-DuU62zZNE7VOt85b6L6U44fgru+AL8gebFURhBB70sY=";
