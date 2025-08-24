8dfb6f6da6d8ae7f49e84c6d1bb856d7980e7e69
cristianoliveira <contato@cristianoliveira.com.br>
2025-08-24 10:12:25 +0200

    chore: adds gotestsum into dev shell
    (cherry picked from commit 6e80fe315c88a4d43cd6e61be379da5d513f3cdf)

--- flake.nix
+++ flake.nix
@@ -17,6 +17,10 @@
 
             golangci-lint
 
+            # Test runner with good output
+            # USAGE: gotestsum --watch
+            gotestsum
+
             # To create new subcommands, run:
             # cobra-cli add <subcommand-name>
             cobra-cli
