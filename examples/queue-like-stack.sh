1626a86fab724e4184a038bd999453a8d883e2bc
cristianoliveira <public+github@cristianoliveira.com>
2025-12-06 17:30:07 +0100

    feat(examples): cycling through example like SwayWM behaviour
    Added a new 'next' command to the scratchpad queue script example
    
    Detailed Changes:
     - Implementation:
       - Added a 'next' command that moves all floating windows into the queue and pops the next window.
       - Extracted push and pop logic into separate functions for reuse.
       - Updated usage message to include the new 'next' command.
     - Documentation:
       - Added a comment noting the script's dependency on jq.
    
    (cherry picked from commit fc2be6167d2f906a581234a9237e8e83018288af)

--- examples/queue-like-stack.sh
+++ examples/queue-like-stack.sh
@@ -6,47 +6,46 @@ touch "$QUEUE" 2>/dev/null || true
 COMMAND=$1
 
 if [ -z "$COMMAND" ]; then
-  echo "Usage: $0 <push|pop|next>"
+  echo "Usage: $0 <push|pop|cycle>"
   exit 1
 fi
 
-function push_into() {
+function push_into_stack() {
   aerospace-scratchpad move --output=json \
     | jq -r 'select(.result=="ok") | .window_id' \
     >> "$QUEUE"
 }
 
-function pop_from() {
+function pop_from_stack() {
   wid=$(head -n1 "$QUEUE")
   tail -n +2 "$QUEUE" > "$QUEUE.tmp" && mv "$QUEUE.tmp" "$QUEUE"
   aerospace-scratchpad show ".+" --filter window-id="^${wid}$" --output=json
 }
 
+function push_all_scratchpads_into_stack() {
+  aerospace-scratchpad move --all-floating -o json |
+    jq -r 'select(.result=="ok") | .window_id' >> "$QUEUE"
+}
+
 if [ "$COMMAND" = "push" ]; then
-  # Push IDs of moved windows
-  push_into
+  push_into_stack
   exit 0
 fi
 
 if [ "$COMMAND" = "pop" ]; then
-  # Pop oldest window ID and summon it back
   if [ -s "$QUEUE" ]; then
-    pop_from
+    pop_from_stack
   else
     echo "Queue empty"
   fi
 fi
 
-# This is implements a similar behavior as i3/SwayWM:
-# `bindsym $mod+minus scratchpad show` 
-if [ "$COMMAND" = "next" ]; then
+# This implements a similar behavior as i3/SwayWM
+# `bindsym $mod+minus scratchpad show` which cycles through the stack
+if [ "$COMMAND" = "cycle" ]; then
   if [ -s "$QUEUE" ]; then
-    # First send all floating windows back
-    aerospace-scratchpad move --all-floating -o json |
-      jq -r 'select(.result=="ok") | .window_id' >> "$QUEUE"
-
-    # Then pop the next window
-    pop_from
+    push_all_scratchpads_into_stack
+    pop_from_stack
   else
     echo "Queue empty"
   fi
